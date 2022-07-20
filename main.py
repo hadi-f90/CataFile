c5318ff37350ba0d4bb4479b6d55d68e0bc5593d
Hadi F <62212402+hadi-f90@users.noreply.github.com>
2022-07-20 21:07:22 +0430

    changed constant names

--- main.py
+++ main.py
@@ -12,16 +12,16 @@ from . import ui
 
 
 # ==========main course====================
-source_dir = folder()
-destination = destination_folder()
+SOURCE = folder()
+DESTINATION = destination_folder()
 
 
 def main():
     print(argv[0], log_file.name)
-    for _ in source_dir.walker():
-        print(source_dir.selected_file_name, _, log_file.name, argv[0])
+    for _ in SOURCE.walker():
+        print(SOURCE.selected_file_name, _, log_file.name, argv[0])
         # if the sellected file is the current app , then forget about it
-        # To d: now that I have multiple app files, I need change it
+        # To d: now that I have multiple app files, I need change itP
         if _ in {log_file.name, _ == argv[0]}:
             # print(_)
             continue
@@ -38,9 +38,9 @@ def process_file(_):
     if os.path.isfile(f.path) and f.check_integerity():
         try:
             print(f.mime, '\n', f.path)
-            if f.mime not in destination.category_dirs.keys():
-                destination.add_category(f.mime)
-            f.move(destination.category_dirs[f.mime])
+            if f.mime not in DESTINATION.category_dirs.keys():
+                DESTINATION.add_category(f.mime)
+            f.move(DESTINATION.category_dirs[f.mime])
 
         except (TypeError, shutil.Error):
             logger(msg=f'An Error occured during processing {_}.\n \
@@ -51,9 +51,9 @@ def process_file(_):
             logger(msg=f'An Error occured during processing {_}.\n \
                     Unknown File')
 
-        destination.add_category('/Corrupted')
+        DESTINATION.add_category('/Corrupted')
         try:
-            f.move(destination.category_dirs['/Corrupted'])
+            f.move(DESTINATION.category_dirs['/Corrupted'])
 
         except shutil.Error:
             logger(msg=f'There was an error copying {f.name}. \
@@ -66,7 +66,7 @@ def process_file(_):
 
 def empty_folder_delete():
     # ===== To do: Delete empty folders after moving files to categories
-    for _ in os.walk(source_dir.current_dir):
+    for _ in os.walk(SOURCE.current_dir):
         if os.path. len(os.listdir(_)) < 1:
             try:
                 logger(msg=f'Removing {_}')
