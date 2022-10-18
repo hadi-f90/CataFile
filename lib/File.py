a3688c1ebd3fa1278546f3d9d2c5c39c8ab0f40e
Hadi F <62212402+hadi-f90@users.noreply.github.com>
2022-10-18 11:46:17 +0330

    Testing & Debugging File module
    copy & move function still have problems with none-existing directories

--- lib/File.py
+++ lib/File.py
@@ -1,5 +1,6 @@
 """This file contains a set of classes used to manipulate different file types."""
 import os
+import pathlib
 import shutil
 
 import fleep
@@ -26,7 +27,9 @@ def create_proper_file_instance():
 
 
 # ============================== file object class ============================
-class File():
+
+
+class File:
     """A class to manipulate  files."""
 
     def __init__(self, file_object):
@@ -37,38 +40,65 @@ class File():
         """
         # Todo: use pathlib & glob for later versions
         # file type info
-        self.path = os.path.abspath(file_object)
-        self.file_object = open(file_object, 'rb')
-        self.file_name, self.extension = os.path.splitext(self.path)
+        self.full_path = pathlib.PurePath(file_object)
+        self.parent_dir = self.full_path.parent
+        self.full_file_name = self.full_path.name
+        self.extension = self.full_path.suffix
+        self.file_name = self.full_path.stem
+
+        self.file_object = open(file_object, "rb")
         self.file_header = self.file_object.read(2048)
 
-        if preferences.get('file_processor') == 0:
+        if preferences.get("file_processor") == 0:
             self.fleep_detect()
 
-        elif preferences.get('file_processor') == 1:
+        elif preferences.get("file_processor") == 1:
             self.magic_detect()
 
-        elif preferences.get('file_processor') == 2:
+        elif preferences.get("file_processor") == 2:
             self.mime = self.extension
 
         else:
-            LOGGER.error('File_processor not set')
+            LOGGER.error("File_processor not set")
             # self.extension_revert()
+        LOGGER.debug(
+            """Class file data:
+                     fullpath:%s
+                     parent_dir: %s
+                     full file name: %s
+                     extension: %s
+                     file name: %s
+                     file object: %s 
+                     file header:\n
+                     %s
+                     mime: %s
+                     file info: %s""",
+            self.full_path,
+            self.parent_dir,
+            self.full_file_name,
+            self.extension,
+            self.file_name,
+            self.file_object,
+            self.file_header,
+            self.mime,
+            self.file_info,
+        )
 
     def magic_detect(self):
         """Detect file type using magic module."""
         self.mime = magic.from_buffer(self.file_header, mime=True)
-        self.type, self.detected_extension = self.mime.split('/')
-        if self.mime == ('', None):
-            self.mime = 'etc'
+        self.type, self.detected_extension = self.mime.split("/")
+        self.detected_extension = "." + self.detected_extension
+        if self.mime == ("", None):
+            self.mime = "etc"
 
     def fleep_detect(self):
         """Detect file type using fleep module."""
         self.file_info = fleep.get(self.file_header)
         self.type = self.file_info.type[0]
-        self.detected_extension = self.file_info.extension[0]
+        self.detected_extension = "." + self.file_info.extension[0]
         self.mime = self.file_info.mime
-        self.mime = 'etc' if len(self.mime) < 1 else self.file_info.mime[0]
+        self.mime = "etc" if len(self.mime) < 1 else self.file_info.mime[0]
 
     def file_date_time(self):
         """
@@ -77,9 +107,11 @@ class File():
         output: (accessed, created, modified)
         """
         # To-do: returns file data and time
-        return (os.path.getatime(self.path),
-                os.path.getctime(self.path),
-                os.path.getmtime(self.path))
+        return (
+            os.path.getatime(self.full_path),
+            os.path.getctime(self.full_path),
+            os.path.getmtime(self.full_path),
+        )
 
     def move(self, destination):
         """Move file to  given destination.
@@ -87,14 +119,25 @@ class File():
         Args:
             destination : str or path-like object
         """
+        LOGGER.debug(
+            "variable data:\nself path: %s\ndestination: %s\n",
+            self.full_path,
+            destination,
+        )
+        destination = self._path_check(destination)
         try:
-            shutil.move(self.path, destination)
-            LOGGER.info('Moved %s to %s.', self.path, destination)
-            self.path = destination + self.file_name + self.extension
+            shutil.move(self.full_path, destination)
+            self.full_path = destination
+            LOGGER.info("Moved %s to %s.", self.full_path, destination)
 
-        except Exception as e:
-            LOGGER.error('Error moving %s to %s.', self.path, destination)
-            LOGGER.error(e)
+        except Exception as error:
+            LOGGER.error(
+                "Error moving %s to %s because of:\n%s.",
+                self.full_path,
+                destination,
+                error,
+            )
+            LOGGER.exception("Deatiled exception: %s", error)
 
     def copy(self, destination):
         """Copy file to given destination.
@@ -102,8 +145,20 @@ class File():
         Args:
             destination : str or path-like object
         """
-        shutil.copy(self.path, destination)
-        LOGGER.info('%s copied to %s', self.path, destination)
+        destination = self._path_check(destination)
+        LOGGER.debug(
+            "variable data:\nself path: %s\ndestination: %s\n",
+            self.full_path,
+            destination,
+        )
+        shutil.copy(self.full_path, destination)
+        LOGGER.info("%s copied to %s", self.full_path, destination)
+
+    def _path_check(self, path):
+        if not pathlib.Path(path).exists():
+            os.mkdir(path)
+        LOGGER.debug("Destination changed from %s to %s", path, path)
+        return path
 
     def rename(self, new_name):
         """
@@ -111,39 +166,42 @@ class File():
 
         new_name: str
         """
-        LOGGER.info('Changing %s name to %s', self.path, new_name)
+        LOGGER.info("Changing %s name to %s", self.full_path, new_name)
         try:
-            os.rename(self.path, new_name)
+            pathlib.Path(self.full_path).rename(new_name)
 
         except FileExistsError as error:
             LOGGER.debug(error)
-            LOGGER.info('%s already exists. Numbering ...', new_name)
+            LOGGER.info("%s already exists. Numbering ...", new_name)
 
             number = 0
-            new_file_name = f'{self.file_name}({number}).{self.extension}'
-            while os.path.exists(new_file_name):
+            new_file_name = f"{self.file_name}({number}).{self.extension}"
+            while pathlib.Path(new_file_name).exists():
                 number += 1
-                new_file_name = f'{self.file_name}({number}).{self.extension}'
-            os.rename(self.path, new_file_name)
-            LOGGER.info('%s renamed to %s', self.path, new_file_name)
+                new_file_name = f"{self.file_name}({number}).{self.extension}"
+            pathlib.Path(self.full_path).rename(new_file_name)
+            LOGGER.info("%s renamed to %s", self.full_path, new_file_name)
 
     def extension_revert(self):
         """Revert the extension of the file to the original one."""
         # if the file type is not known then try fo file its type
-        if self.extension in (None, '') or self.detected_extension != self.extension:
-            LOGGER.info('Changing %s extension to %s', self.path, self.detected_extension)
-            new_name = f'{self.file_name}.{self.detected_extension}'
+        if self.extension in (None,
+                              "") or self.detected_extension != self.extension:
+            LOGGER.info("Changing %s extension to %s", self.full_path,
+                        self.detected_extension)
+            new_name = self.full_path.with_suffix(self.detected_extension)
             self.rename(new_name)
             self.extension = self.detected_extension
-            self.path = os.path.abspath(new_name)
+            self.full_path = pathlib.PurePath(new_name)
 
     def __str__(self) -> str:
         """Return string representation name of the class instance."""
-        return self.path
+        return self.full_path
 
 
 class FontFile(File, Font):
     """Special class for font file actions that inherits MyFile & Font methods."""
+
     def __init__(self, file_object=None):
         """Initialize the font file actions."""
         super().__init__(file_object)
@@ -151,15 +209,15 @@ class FontFile(File, Font):
         self.extension = self.font.get_format()
         self.font_name = self.font.get_name(key=Font.NAME_FAMILY_NAME)
         self.version = self.font.get_version()
-        self.weight = self.font.get_weight()['name']
+        self.weight = self.font.get_weight()["name"]
 
         # self.revert_font_name()
 
     def revert_font_name(self):  # not testes Yet!
         """Revert font file name to its internal name."""
-        new_font_name = f'{self.font_name}-{self.weight}.{self.extension}'
+        new_font_name = f"{self.font_name}-{self.weight}.{self.extension}"
         self.rename(new_font_name)
-        LOGGER.info('Font reverted to its name: %s', self.file_name)
+        LOGGER.info("Font reverted to its name: %s", self.file_name)
         self.file_name = new_font_name
 
 
@@ -167,20 +225,19 @@ class ArchiveFile(File):
     """Archive File Type special class."""
 
     def __init__(self, file_object=None):
-        """Initialize a ArchiveFile object based File
-        """
+        """Initialize a ArchiveFile object based File"""
         super().__init__(file_object)
         # self.test_archive()
 
     def test_archive(self):
         """Test archive file health."""
         try:
-            patoolib.test_archive(self.path)
-            LOGGER.debug('Testing file %s', self.path)
+            patoolib.test_archive(self.full_path)
+            LOGGER.debug("Testing file %s", self.full_path)
             return True
         except patoolib.util.PatoolError as error:
-            LOGGER.error(
-                'Error. Testing %s Failed with error %s', self.path, error)
+            LOGGER.error("Error. Testing %s Failed with error %s",
+                         self.full_path, error)
             return False
 
     def check_integerity(self):
@@ -188,13 +245,15 @@ class ArchiveFile(File):
         if self.extension in patoolib.ArchiveFormats:
             return self.test_archive()
         # not tested for documents & apks
-        if regex.search(
-            r'.doc[x|m]?|.xls[x|m]?|.pp[t|s|x|m]+| \
-                .od[b|c|f|g|m|p|t|s]+|.ap*2[k|x]+',
+        if (regex.search(
+                r".doc[x|m]?|.xls[x|m]?|.pp[t|s|x|m]+| \
+                .od[b|c|f|g|m|p|t|s]+|.ap*2[k|x]+",
                 self.extension,
-                flags=regex.IGNORECASE) is not None:
-            test_case = shutil.copyfile(self.path, f'{self.file_name}.zip')
-            print(os.path.abspath(test_case))
+                flags=regex.IGNORECASE,
+        ) is not None):
+            test_case = shutil.copyfile(self.full_path,
+                                        f"{self.file_name}.zip")
+            print(os.full_path.abspath(test_case))
             result = self.test_archive()
             os.remove(test_case)
             return result
--- tests/test_file.py
+++ tests/test_file.py
@@ -1,19 +1,22 @@
 #!/usr/bin/env python
 """Test cases for File class in File.py."""
 import os
+from pathlib import PurePath
 from pprint import pprint
 from random import choice
 from sys import path
 
-path.append('..')
 import fleep
+
 from config import pref
 from lib import File
 
-SOURCE_TEST_ADDRESS = '/home/hadi/Documents/GitHub/CataFile/tests/test_cases'
-DEST_TEST_ADDRESS = '/home/hadi/Documents/GitHub/CataFile/tests/test_cases/dest'
-COPY_DEST = f'{DEST_TEST_ADDRESS}/copy'
-MOVE_DEST = f'{DEST_TEST_ADDRESS}/move'
+path.append("..")
+
+SOURCE_TEST_ADDRESS = "/home/hadi/Documents/GitHub/CataFile/tests/test_cases"
+DEST_TEST_ADDRESS = "/home/hadi/Documents/GitHub/CataFile/tests/test_cases/dest"
+COPY_DEST = f"{DEST_TEST_ADDRESS}/copy"
+MOVE_DEST = f"{DEST_TEST_ADDRESS}/move"
 os.chdir(SOURCE_TEST_ADDRESS)
 
 
@@ -42,33 +45,109 @@ files_list = populate_list_of_files(SOURCE_TEST_ADDRESS)
 pprint(files_list)
 
 # ==============MyFile Tests ====================
-testcase_my_file = File.File(choice(files_list))
-print(0, testcase_my_file)
-print(1, testcase_my_file.file_name)
-print(2, testcase_my_file.extension)
-print(3.1, testcase_my_file.path)  # full path includes name no need 2 fullname
-print(3.2, ["Fleep", "Magic", "Extension"][pref.get('file_processor')])
-print(3.3, testcase_my_file.file_object.read(2048))
-print(3.4, fleep.get(testcase_my_file.file_object.read(2048)))
-print(4.0, testcase_my_file.fleep_detect())
-print(4.1, testcase_my_file.file_info)
-print(4.2, testcase_my_file.type)
-print(4.3, testcase_my_file.detected_extension)
-print(4.4, testcase_my_file.file_info.mime)
-print(5.1, testcase_my_file.magic_detect())
-print(5.2, testcase_my_file.mime)
-print(5.3, testcase_my_file.type)
-print(5.4, testcase_my_file.detected_extension)
-print(6, testcase_my_file.file_date_time())
-testcase_my_file.fleep_detect()
-print(7, testcase_my_file.extension_revert())
-print(8.0, testcase_my_file.copy(COPY_DEST))
-print(8.2, os.path.exists(COPY_DEST + testcase_my_file.path))
-print(8.3, testcase_my_file.move(MOVE_DEST))
-print(8.4, os.path.exists(testcase_my_file.path))
+# ==============MyFile Tests ====================
+c = choice(files_list)
+testcase_file = File.File(c)
 
 
-def test_file_tester(self):
+def test_file_init():  # passed!
+    """Check initializing test file module."""
+    return isinstance(testcase_file, File.File)
+
+
+def test_get_file_name():
+    """Check file name representation."""
+    return testcase_file.full_file_name == c
+
+
+def test_get_file_extension():
+    """Check file extension."""
+    extension = c[c.index(".") + 1:]
+    return extension == testcase_file.extension
+
+
+print(
+    testcase_file.parent_dir,
+    PurePath("/home/hadi/Documents/GitHub/test center/CataFile/test_cases"),
+)
+
+
+def test_get_parent_folder(
+):  # In logs it represnts only  one dot! why and how?!
+    """Check parent folder representation"""
+    return testcase_file.parent_dir == PurePath(
+        "/home/hadi/Documents/GitHub/test center/CataFile/test_cases"
+    )  # full path includes name no need 2 fullname
+
+
+def test_get_full_file_name():
+    return testcase_file.full_file_name == c
+
+
+def test_reading_file_header():  # Passed!
+    return testcase_file.file_header == testcase_file.file_object.read(2048)
+
+
+def test_file_date_time_function():  # Passed!
+    return (
+        testcase_file.file_date_time() == os.path.getatime(
+            testcase_file.full_path),
+        os.path.getctime(testcase_file.full_path),
+        os.path.getmtime(testcase_file.full_path),
+    )
+
+
+# ================fleep_detect tests======================
+print(3.2, ["Fleep", "Magic", "Extension"][pref.get("file_processor")])
+
+
+def test_file_detection_with_fleep():
+    return (fleep.get(
+        testcase_file.file_object.read(2048)) == testcase_file.fleep_detect())
+
+
+def test_file_info():
+    print(4.1, testcase_file.file_info)
+
+
+def test_file_type():
+    print(4.2, testcase_file.type)
+
+
+def test_file_detection_function_extension():
+    print(4.3, testcase_file.detected_extension)
+
+
+def test_file_detection_function_mime():
+    print(4.4, testcase_file.file_info.mime)
+
+
+# ================maigc module test=====================
+
+
+def test_file_detection_with_magic():
+    print(5.1, testcase_file.magic_detect())
+    print(5.2, testcase_file.mime)
+    print(5.3, testcase_file.type)
+    print(5.4, testcase_file.detected_extension)
+
+
+def test_reverting_file_extension_function():
+    print(7, testcase_file.extension_revert())
+
+
+def test_file_copy_function():
+    testcase_file.copy(COPY_DEST)
+    os.path.exists(testcase_file.full_path)
+
+
+def test_move_function(
+):  # Has problems with not existing directory, same is true about copy function
+    testcase_file.move(MOVE_DEST)
+    return os.path.exists(testcase_file.full_path)
+
+
+def test_file_tester():
     # assert testcase_my_file.__test_archive() is True
     pass
 
